2575e8bd8398f74ae1a098777ccad6b62b12f2b5
Anug Saha <anugsaha@gmail.com>
2022-08-08 11:16:30 -0400

    activity controller

---
+++ src/main/java/com/example/demo/controller/ActivityController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Activity;
+import com.example.demo.service.ActivityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/activities")
+public class ActivityController {
+    @Autowired
+    private ActivityService activityService;
+
+    @GetMapping
+    public List<Activity> readActivities(){
+        return activityService.getActivities();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteActivity(int id){
+        activityService.deleteActivity(id);
+    }
+
+}
